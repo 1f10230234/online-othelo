36e88df2fae887221a3d0e5f1ce4da247b426509
1f10230234 <s1f102302348@iniad.org>
2023-06-29 08:51:11 +0900

    ファイルをきれいに↓したしｔしｓｈｓ

--- server/api/rooms/controller.ts
+++ server/api/rooms/controller.ts
@@ -5,15 +5,9 @@ export default defineController(() => ({
   get: async () => ({
     status: 200,
     body: await roomsRepository.findLatest(),
-    // {board: boardUseCase.getBoard().exBoard,
-    // count: boardUseCase.getBoard().exCount,
-    // turn: boardUseCase.getBoard().exTurn,
-    // passCount: boardUseCase.getBoard().exPassCount,
-    // },
   }),
   post: async () => ({
     status: 201,
     body: await roomUsecase.create(),
-    // {board: boardUseCase.clickBoard(body, user.id),},
   }),
 }));
